c89cba2f01e9c29f8e947d035661fc580f7c4191
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-11-26 11:21:14 +0200

    Add separate file for prisma

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-import { PrismaClient } from '@prisma/client'
+import prisma from './lib/prisma'
 
 const app = express()
-const prisma = new PrismaClient()
 
 app.use(cors())
 app.use(express.json())
